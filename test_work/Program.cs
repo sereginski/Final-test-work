d69b7361c857e2d67855eb2598ed34efece2d03a
sereginski <relict1900@bk.ru>
2023-01-04 19:40:56 +0300

    решение1

--- test_work/Program.cs
+++ test_work/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// test work
+Console.Clear();
+string[] array = {"Happy", "New", "Year", "2023", "Hi"};
+string[] newArray = new string[array.Length];
+
+void InputNewArray(string[] array, string[] newArray)
+{
+   int count = 0;
+   for(int i = 0; i < array.Length; i++)
+   {
+       if(array[i].Length <= 3)
+       {
+          newArray[count] = array[i];
+          count++;
+       }
+   }
+}
+   
+void Print(string[] mas)
+{
+  for(int i = 0; i < mas.Length; i++)
+  {
+     Console.WriteLine($"{mas[i]}");
+  }
+  Console.WriteLine();
+}
+InputNewArray(array, newArray);
+Print(newArray);
